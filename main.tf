1082ab4bfab28ee478a4ebfe6869445af548abb3
R-Owino <sayeakinyi@gmail.com>
2024-11-23 00:28:46 +0300

    feat: main entry file for configurations

---
+++ main.tf
@@ -0,0 +1,44 @@
+# main entry file for terraform
+
+module "cognito" {
+  source = "./modules/cognito"
+
+  project_name        = var.project_name
+  environment         = var.environment
+  lambda_function_arn = module.lambda.userdata_function_arn
+  dynamodb_table_name = "userdata"
+}
+
+module "dynamodb" {
+  source = "./modules/dynamodb"
+
+  project_name                  = var.project_name
+  environment                   = var.environment
+  userdata_table_name           = "userdata"
+  documents_metadata_table_name = "documents-metadata"
+}
+
+module "lambda" {
+  source = "./modules/lambda"
+
+  project_name                 = var.project_name
+  environment                  = var.environment
+  dynamodb_userdata_table_name = module.dynamodb.table_name
+  dynamodb_userdata_table_arn  = module.dynamodb.table_arn
+  dynamodb_documents_metadata_table_name = module.dynamodb.documents_metadata_table_name
+  dynamodb_documents_metadata_table_arn  = module.dynamodb.documents_metadata_table_arn
+  s3_bucket_name                         = module.s3.bucket_name
+  s3_bucket_arn                          = module.s3.bucket_arn
+}
+
+module "api_gateway" {
+  source = "./modules/api_gateway"
+
+  project_name                  = var.project_name
+  environment                   = var.environment
+  userpool_arn                  = module.cognito.userpool_arn
+  upload_lambda_invoke_arn      = module.lambda.upload_function_arn
+  download_lambda_invoke_arn    = module.lambda.download_function_arn
+  upload_lambda_function_name   = module.lambda.upload_function_name
+  download_lambda_function_name = module.lambda.download_function_name
+}
