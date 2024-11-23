69532ff05fbaec3d9dbe7f1de5189937abf6f538
R-Owino <sayeakinyi@gmail.com>
2024-11-23 23:23:02 +0300

    refactor: add S3 module

--- main.tf
+++ main.tf
@@ -21,10 +21,10 @@ module "dynamodb" {
 module "lambda" {
   source = "./modules/lambda"
 
-  project_name                 = var.project_name
-  environment                  = var.environment
-  dynamodb_userdata_table_name = module.dynamodb.table_name
-  dynamodb_userdata_table_arn  = module.dynamodb.table_arn
+  project_name                           = var.project_name
+  environment                            = var.environment
+  dynamodb_userdata_table_name           = module.dynamodb.table_name
+  dynamodb_userdata_table_arn            = module.dynamodb.table_arn
   dynamodb_documents_metadata_table_name = module.dynamodb.documents_metadata_table_name
   dynamodb_documents_metadata_table_arn  = module.dynamodb.documents_metadata_table_arn
   s3_bucket_name                         = module.s3.bucket_name
@@ -41,4 +41,11 @@ module "api_gateway" {
   download_lambda_invoke_arn    = module.lambda.download_function_arn
   upload_lambda_function_name   = module.lambda.upload_function_name
   download_lambda_function_name = module.lambda.download_function_name
+}
+
+module "s3" {
+  source = "./modules/s3"
+
+  project_name = var.project_name
+  environment  = var.environment
 }
