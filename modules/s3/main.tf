03fa9b5a3bafeafe453f817f89e2f3fc32d3ef64
R-Owino <sayeakinyi@gmail.com>
2024-11-23 23:21:56 +0300

    feat: S3 module: - creates a bucket to store documents - has a policy that allows lambda interact with the bucket

---
+++ modules/s3/main.tf
@@ -0,0 +1,30 @@
+/* S3 resources */
+
+resource "aws_s3_bucket" "documents" {
+  bucket = "${var.project_name}-documents"
+
+    tags = {
+      Name       = "${var.project_name}-documents",
+        Environment = var.environment
+    }
+}
+
+resource "aws_s3_bucket_acl" "documents" {
+  bucket = aws_s3_bucket.documents.id
+  acl    = "private"
+  
+}
+
+resource "aws_s3_bucket_versioning" "documents-versioning" {
+  bucket = aws_s3_bucket.documents.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.documents.id
+  policy = templatefile("${path.module}/bucket-policy.json", {
+    bucket_arn = aws_s3_bucket.documents.arn
+  })
+}
