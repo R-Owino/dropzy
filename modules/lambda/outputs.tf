3686f7711a27a7fbdcf2bee3d2155ede3bbd929c
R-Owino <sayeakinyi@gmail.com>
2024-11-23 23:11:52 +0300

    chore: output upload and downloaf function names for API gateway variables

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -5,9 +5,19 @@ output "userdata_function_arn" {
   value       = aws_lambda_function.userdata.arn
 }
 
+output "upload_function_name" {
+  description = "Name of the upload Lambda function"
+  value       = aws_lambda_function.upload.function_name
+}
+
 output "upload_function_arn" {
   description = "ARN of the upload Lambda function"
   value       = aws_lambda_function.upload.arn
+}
+
+output "download_function_name" {
+  description = "Name of the download Lambda function"
+  value       = aws_lambda_function.download.function_name
 }
 
 output "download_function_arn" {
