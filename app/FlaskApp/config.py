5a63f91d5a98c5f7e116297adc8f178f9b0b6b50
R-Owino <sayeakinyi@gmail.com>
2024-11-29 22:17:09 +0300

    feat: app configurations

---
+++ app/FlaskApp/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    """app configurations"""
+    SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "xyz-xyz-xyz")
+    AWS_REGION = os.getenv("AWS_REGION", "us-west-2")
+    AWS_COGNITO_USER_POOL_ID = os.getenv("AWS_COGNITO_USER_POOL_ID")
+    AWS_COGNITO_CLIENT_ID = os.getenv("AWS_COGNITO_CLIENT_ID")
