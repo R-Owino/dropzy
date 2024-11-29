7129f92207f07b6508180cd24fde5a0dbb3299a1
R-Owino <sayeakinyi@gmail.com>
2024-11-29 21:54:31 +0300

    feat: home page route: - default landing page

---
+++ app/FlaskApp/routes/landing.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template
+
+landing_bp = Blueprint("landing", __name__)
+
+@landing_bp.route("/")
+def landing():
+    """defines the landing page route"""
+    return render_template("landing.html")
