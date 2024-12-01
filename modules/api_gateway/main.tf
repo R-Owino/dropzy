9a77a0402c2e3c12fe5d4c90808c7bb81e9ed18b
R-Owino <sayeakinyi@gmail.com>
2024-12-01 23:52:57 +0300

    update: enable cors options for file upload functionality

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -56,6 +56,45 @@ resource "aws_api_gateway_integration" "download_lambda" {
   uri         = var.download_lambda_invoke_arn
 }
 
+resource "aws_api_gateway_method" "cors_options" {
+  rest_api_id   = aws_api_gateway_rest_api.docs.id
+  resource_id   = aws_api_gateway_resource.file-share.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "cors_options" {
+  rest_api_id       = aws_api_gateway_rest_api.docs.id
+  resource_id       = aws_api_gateway_resource.file-share.id
+  http_method       = aws_api_gateway_method.cors_options.http_method
+  type              = "MOCK"
+  request_templates = { "application/json" = "{\"statusCode\": 200}" }
+}
+
+resource "aws_api_gateway_method_response" "cors_options_response" {
+  rest_api_id = aws_api_gateway_rest_api.docs.id
+  resource_id = aws_api_gateway_resource.file-share.id
+  http_method = aws_api_gateway_method.cors_options.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "cors_options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.docs.id
+  resource_id = aws_api_gateway_resource.file-share.id
+  http_method = aws_api_gateway_method.cors_options.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type, Authorization'",
+    "method.response.header.Access-Control-Allow-Methods" = "'POST, OPTIONS'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
 resource "aws_api_gateway_deployment" "docs" {
   rest_api_id = aws_api_gateway_rest_api.docs.id
 
