456a76df567feda28e8c7350fc5bae4d06f001d6
R-Owino <sayeakinyi@gmail.com>
2024-12-01 23:46:32 +0300

    add: API Gateway URL

--- app/FlaskApp/config.py
+++ app/FlaskApp/config.py
@@ -9,3 +9,4 @@ class Config:
     AWS_REGION = os.getenv("AWS_REGION", "us-west-2")
     AWS_COGNITO_USER_POOL_ID = os.getenv("AWS_COGNITO_USER_POOL_ID")
     AWS_COGNITO_CLIENT_ID = os.getenv("AWS_COGNITO_CLIENT_ID")
+    API_GATEWAY_URL = os.getenv("AWS_API_GATEWAY_URL")
