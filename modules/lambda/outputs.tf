12072ffbb0092ae2d2ede9dc88e69d9582430e61
R-Owino <sayeakinyi@gmail.com>
2024-12-05 12:20:12 +0300

    update: output the file metadata function name

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -20,6 +20,11 @@ output "download_function_name" {
   value       = aws_lambda_function.download.function_name
 }
 
+output "file_metadata_lambda_function_name" {
+  description = "Name of the file metadata fetching function"
+  value = aws_lambda_function.fetch_file_metadata
+}
+
 output "download_function_arn" {
   description = "ARN of the download Lambda function"
   value       = aws_lambda_function.download.arn
