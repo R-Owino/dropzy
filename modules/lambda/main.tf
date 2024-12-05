93ce410a94f8e339d266d129516299197861d116
R-Owino <sayeakinyi@gmail.com>
2024-12-05 12:17:24 +0300

    update: add a resource for a lambda function that fetches file metadata with it's appropriate permissions

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -12,6 +12,12 @@ data "archive_file" "upload_lambda_zip_file" {
   output_path = "${path.module}/upload_lambda.zip"
 }
 
+data "archive_file" "fetch_file_metadata_zip_file" {
+  type = "zip"
+  source_file = "${path.module}/file_metadata_lambda.py"
+  output_path = "${path.module}/file_metadata_lambda.zip"
+}
+
 data "archive_file" "download_lambda_zip_file" {
   type        = "zip"
   source_file = "${path.module}/download_lambda.py"
@@ -49,6 +55,21 @@ resource "aws_lambda_function" "upload" {
   }
 }
 
+resource "aws_lambda_function" "fetch_file_metadata" {
+  filename = "${path.module}/file_metadata_lambda.zip"
+  function_name = "${var.project_name}-fetch_metadata-${var.environment}"
+  role = aws_iam_role.lambda_role
+  handler = "file_metadata_lambda.handler"
+  runtime = "python3.12"
+  timeout = 30
+
+  environment {
+    variables = {
+      DYNAMODB_TABLE_NAME = var.dynamodb_documents_metadata_table_name
+    }
+  }
+}
+
 resource "aws_lambda_function" "download" {
   filename      = "${path.module}/download_lambda.zip"
   function_name = "${var.project_name}-download-${var.environment}"
@@ -81,6 +102,15 @@ resource "aws_iam_role_policy" "lambda_dynamodb" {
 
   policy = templatefile("${path.module}/lambda-dynamodb-policy.json", {
     dynamodb_userdata_table_arn           = var.dynamodb_userdata_table_arn,
+    dynamodb_documents_metadata_table_arn = var.dynamodb_documents_metadata_table_arn
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_dynamo_metadata" {
+  name = "${var.project_name}-lambda-dynamodb-metadata-policy-${var.environment}"
+  role = aws_iam_role.lambda_role
+
+  policy = templatefile("${path.module}/lambda-dynamodb-policy.json", {
     dynamodb_documents_metadata_table_arn = var.dynamodb_documents_metadata_table_arn
   })
 }
