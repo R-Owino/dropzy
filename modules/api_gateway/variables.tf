49dcdf4bfa33d7a0f8a835dc679e1f8f721c24d2
R-Owino <sayeakinyi@gmail.com>
2024-12-05 12:04:53 +0300

    update: add variables for file metadata lambda function

--- modules/api_gateway/variables.tf
+++ modules/api_gateway/variables.tf
@@ -25,6 +25,11 @@ variable "download_lambda_invoke_arn" {
   type        = string
 }
 
+variable "file_metadata_invoke_arn" {
+  description = "Invoke ARN of the fetch file metadata lambda function"
+  type = string
+}
+
 variable "upload_lambda_function_name" {
   description = "Name of the upload Lambda function"
   type        = string
@@ -33,4 +38,9 @@ variable "upload_lambda_function_name" {
 variable "download_lambda_function_name" {
   description = "Name of the download Lambda function"
   type        = string
+}
+
+variable "file_metadata_lambda_function_name" {
+  description = "Name of the fetch file metadata lambda function"
+  type = string
 }
