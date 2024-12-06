f98afaf31ce7c2186cff6605273c826e9e6fa92f
R-Owino <sayeakinyi@gmail.com>
2024-12-06 23:57:00 +0300

    update: rename the project componenets appropriately and add lifecycle policies for the objects

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,30 +1,44 @@
 /* S3 resources */
 
-resource "aws_s3_bucket" "documents" {
-  bucket = "${var.project_name}-documents"
+resource "aws_s3_bucket" "files" {
+  bucket = "${var.project_name}-files"
 
   tags = {
-    Name        = "${var.project_name}-documents",
+    Name        = "${var.project_name}-files",
     Environment = var.environment
   }
 }
 
-resource "aws_s3_bucket_acl" "documents" {
-  bucket = aws_s3_bucket.documents.id
+resource "aws_s3_bucket_acl" "files" {
+  bucket = aws_s3_bucket.files.id
   acl    = "private"
 
 }
 
-resource "aws_s3_bucket_versioning" "documents-versioning" {
-  bucket = aws_s3_bucket.documents.id
+resource "aws_s3_bucket_versioning" "files-versioning" {
+  bucket = aws_s3_bucket.files.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.documents.id
+  bucket = aws_s3_bucket.files.id
   policy = templatefile("${path.module}/bucket-policy.json", {
-    bucket_arn = aws_s3_bucket.documents.arn
+    bucket_arn = aws_s3_bucket.files.arn
   })
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "files_lifecycle" {
+  bucket = aws_s3_bucket.files.id
+
+  rule {
+    id = "move-to-intelliget-tiering"
+    status = "Enabled"
+
+    transition {
+      days = 90
+      storage_class = "INTELLIGENT_TIERING"
+    }
+  }
 }
