2be6c392fbabb0869105199b7fbfb6e3746e26ee
R-Owino <sayeakinyi@gmail.com>
2024-12-07 00:02:40 +0300

    update: add the outputs for the delete lambda function

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -1,7 +1,7 @@
 /* lambda functions outputs */
 
 output "userdata_function_arn" {
-  description = "ARN of the Lambda function"
+  description = "ARN of the userdata lambda function"
   value       = aws_lambda_function.userdata.arn
 }
 
@@ -11,23 +11,38 @@ output "upload_function_name" {
 }
 
 output "upload_function_arn" {
-  description = "ARN of the upload Lambda function"
+  description = "ARN of the upload lambda function"
   value       = aws_lambda_function.upload.arn
 }
 
 output "download_function_name" {
-  description = "Name of the download Lambda function"
+  description = "Name of the download lambda function"
   value       = aws_lambda_function.download.function_name
 }
 
-output "file_metadata_lambda_function_name" {
-  description = "Name of the file metadata fetching function"
-  value = aws_lambda_function.fetch_file_metadata
+output "download_function_arn" {
+  description = "ARN of the download lambda function"
+  value       = aws_lambda_function.download.arn
 }
 
-output "download_function_arn" {
-  description = "ARN of the download Lambda function"
-  value       = aws_lambda_function.download.arn
+output "file_metadata_function_name" {
+  description = "Name of the file metadata fetching function"
+  value = aws_lambda_function.fetch_file_metadata.function_name
+}
+
+output "file_metadata_arn" {
+  description = "ARN of the file metadata lambda function"
+  value = aws_lambda_function.fetch_file_metadata.arn
+}
+
+output "delete_lambda_function_name" {
+  description = "Name of the delete lambda function"
+  value = aws_lambda_function.delete_file.function_name
+}
+
+output "delete_function_arn" {
+  description = "ARN of the delete file lambda function"
+  value = aws_lambda_function.delete_file.arn
 }
 
 output "lambda_execution_role_arn" {
