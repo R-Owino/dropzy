8da0c9238ebf929e534b57ac252024472cac03ff
R-Owino <sayeakinyi@gmail.com>
2024-12-09 23:04:22 +0300

    chore: format the file for alignment

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,9 +1,9 @@
 output "bucket_name" {
   description = "Name of the S3 bucket"
-  value       = aws_s3_bucket.documents.bucket
+  value       = aws_s3_bucket.files.bucket
 }
 
 output "bucket_arn" {
   description = "ARN of the S3 bucket"
-  value       = aws_s3_bucket.documents.arn
+  value       = aws_s3_bucket.files.arn
 }
