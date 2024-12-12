0dee09d9cf7b873859e0535a70c3654e20ca7a42
R-Owino <sayeakinyi@gmail.com>
2024-12-12 23:20:49 +0300

    chore: rename the modules' sources according to the variables in the individual modules

--- main.tf
+++ main.tf
@@ -3,10 +3,11 @@
 module "cognito" {
   source = "./modules/cognito"
 
-  project_name        = var.project_name
-  environment         = var.environment
-  lambda_function_arn = module.lambda.userdata_function_arn
-  dynamodb_table_name = "userdata"
+  project_name           = var.project_name
+  environment            = var.environment
+  userdata_function_arn  = module.lambda.userdata_function_arn
+  userdata_function_name = module.lambda.userdata_function_name
+  dynamodb_table_name    = var.userdata_table_name
 }
 
 module "dynamodb" {
@@ -14,8 +15,8 @@ module "dynamodb" {
 
   project_name                  = var.project_name
   environment                   = var.environment
-  userdata_table_name           = "userdata"
-  documents_metadata_table_name = "documents-metadata"
+  userdata_table_name           = var.userdata_table_name
+  documents_metadata_table_name = var.documents_metadata_table_name
 }
 
 module "lambda" {
@@ -34,17 +35,18 @@ module "lambda" {
 module "api_gateway" {
   source = "./modules/api_gateway"
 
-  project_name                  = var.project_name
-  environment                   = var.environment
-  userpool_arn                  = module.cognito.userpool_arn
-  upload_lambda_invoke_arn      = module.lambda.upload_function_arn
-  download_lambda_invoke_arn    = module.lambda.download_function_arn
-  upload_lambda_function_name   = module.lambda.upload_function_name
-  download_lambda_function_name = module.lambda.download_function_name
+  aws_region                         = var.aws_region
+  project_name                       = var.project_name
+  environment                        = var.environment
+  userpool_arn                       = module.cognito.userpool_arn
+  upload_lambda_arn                  = module.lambda.upload_function_arn
+  download_lambda_arn                = module.lambda.download_function_arn
+  file_metadata_arn                  = module.lambda.file_metadata_arn
+  delete_lambda_arn                  = module.lambda.delete_function_arn
+  upload_lambda_function_name        = module.lambda.upload_function_name
+  download_lambda_function_name      = module.lambda.download_function_name
   file_metadata_lambda_function_name = module.lambda.file_metadata_function_name
-  file_metadata_invoke_arn = module.lambda.file_metadata_arn
-  delete_lambda_function_name = module.lambda.delete_lambda_function_name
-  delete_lambda_invoke_arn = module.lambda.delete_function_arn
+  delete_lambda_function_name        = module.lambda.delete_lambda_function_name
 }
 
 module "s3" {
