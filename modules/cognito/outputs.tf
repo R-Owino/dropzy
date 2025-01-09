92f6e186e9a4f7733b92d6619a5a7369a09002c9
R-Owino <sayeakinyi@gmail.com>
2025-01-09 10:29:02 +0300

    cognito outputs.tf: - remove cognito domain resource output

--- modules/cognito/outputs.tf
+++ modules/cognito/outputs.tf
@@ -8,13 +8,7 @@ output "userpool_arn" {
   value       = aws_cognito_user_pool.userpool.arn
 }
 
-
 output "userpool_client_id" {
   description = "ID of the Cognito user pool client"
   value       = aws_cognito_user_pool_client.userpool_client.id
-}
-
-output "cognito_domain" {
-  description = "Domain prefix of the Cognito User Pool"
-  value       = aws_cognito_user_pool_domain.cognito_domain.domain
 }
