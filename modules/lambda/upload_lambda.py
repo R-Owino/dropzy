08833bebc8d50d6b3ab364aa7e4375e81e474343
R-Owino <sayeakinyi@gmail.com>
2025-01-09 11:54:54 +0300

    update: file upload lambda function: - ensure the payload structure from API gateway is parsed correctly to the lambda function - add logging to help in debugging

--- modules/lambda/upload_lambda.py
+++ modules/lambda/upload_lambda.py
@@ -1,26 +1,37 @@
 """ 
-uploads a file to S3 bucket and
-stores a document metadata in DynamoDB
+uploads a file to S3 bucket
+stores a file's metadata in a DynamoDB table
 """
 
 import boto3
 import json
 import os
 import uuid
-import base64
+import logging
 from datetime import datetime, timezone
 
-def lambda_handler(event, context):
-    try:
-        s3 = boto3.client("s3")
-        dynamodb = boto3.resource("dynamodb")
+logger = logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
-        BUCKET_NAME = os.environ("BUCKET_NAME")
-        TABLE_NAME =  os.environ("TABLE_NAME")
+s3 = boto3.client("s3")
+dynamodb = boto3.resource("dynamodb")
+
+BUCKET_NAME = os.environ["S3_BUCKET_NAME"]
+TABLE_NAME =  os.environ["DYNAMODB_TABLE_NAME"]
+
+def lambda_handler(event, context):
+    logger.info(f"Received event: {json.dumps(event)}")
+
+    try:
+        if isinstance(event["body"], str):
+            body = json.loads(event["body"])
+        else:
+            body = event["body"]
+
+        logger.info(f"Parsed body: {json.dumps("body")}")
 
         # retrieve file content
-        body = json.loads(event["body"])
-        file_content = base64.b64encode(body["file_content"])
+        file_content = body["file_content"]
         file_name = body["file_name"]
         content_type = body.get("content_type", "application/octet-stream")
 
@@ -30,50 +41,48 @@ def lambda_handler(event, context):
         # upload to s3
         s3.put_object(
             Bucket=BUCKET_NAME,
-            KEY=file_key,
+            Key=file_key,
             Body=file_content,
             ContentType=content_type
         )
 
-        # get object URL from s3
-        object_url = f"https://{BUCKET_NAME}.s3.amazonaws.com/{file_key}"
-
         # metadata for dynamodb
         timestamp = datetime.now(timezone.utc).isoformat()
         file_metadata = {
-            'file_id': str(uuid.uuid4()),
+            'DocumentId': str(uuid.uuid4()),
             'file_name': file_name,
             'file_key': file_key,
             'content_type': content_type,
-            'object_url': object_url,
+            'object_url': f"https://{BUCKET_NAME}.s3.amazonaws.com/{file_key}",
             'upload_timestamp': timestamp,
             'size_bytes': len(file_content)
         }
 
         table = dynamodb.Table(TABLE_NAME)
-        table.PUT_ITEM(Item=file_metadata)
+        table.put_item(Item=file_metadata)
 
         return {
-            'StatusCode': 200,
+            'statusCode': 200,
             'headers': {
-                'Contet-Type': 'Application/json',
-                'Allow-Access-Control-Origin': '*',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': 'POST, OPTIONS',
                 'Access-Control-Allow-Headers': 'Content-Type, Authorization'
             },
             'body': json.dumps({
                 'message': 'File uploaded successfully',
-                'file_id': file_metadata['file_id'],
-                'object_url': object_url
+                'DocumentId': file_metadata['DocumentId'],
+                'object_url': file_metadata['object_url']
             })
         }
     
     except Exception as e:
+        print(f"Error: {str(e)}")
         return {
-            'StatusCode': 500,
+            'statusCode': 500,
             'headers': {
-                'Contet-Type': 'Application/json',
-                'Allow-Access-Control-Origin': '*'
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
             },
             'body': json.dumps({
                 'error': str(e)
