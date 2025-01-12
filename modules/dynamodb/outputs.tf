e65dd3140a9df44a611b4fe81fd0a61f00ab2d5b
R-Owino <sayeakinyi@gmail.com>
2025-01-12 23:47:04 +0300

    refactor: give a more descriptive output name for userdata table name and arn outputs

--- modules/dynamodb/outputs.tf
+++ modules/dynamodb/outputs.tf
@@ -1,9 +1,9 @@
-output "table_name" {
+output "userdata_table_name" {
   description = "Name of the DynamoDB table"
   value       = aws_dynamodb_table.userdata.name
 }
 
-output "table_arn" {
+output "userdata_table_arn" {
   description = "ARN of the DynamoDB table"
   value       = aws_dynamodb_table.userdata.arn
 }
