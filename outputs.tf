82f0bbe96d28b7487b9ea54a21849f65b2c328c0
R-Owino <sayeakinyi@gmail.com>
2025-01-15 23:47:26 +0300

    refactor: remove upload URL

--- outputs.tf
+++ outputs.tf
@@ -15,11 +15,6 @@ output "api_gateway_invoke_url" {
   value       = module.api_gateway.api_gateway_invoke_url
 }
 
-output "api_gateway_upload_url" {
-  description = "Invoke URL for uploading files"
-  value       = module.api_gateway.upload_url
-}
-
 output "api_gateway_fetch_metadata_url" {
   description = "Invoke URL for fetching file metadata"
   value       = module.api_gateway.fetch_metadata_url
