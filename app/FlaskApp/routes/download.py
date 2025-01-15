64398e7dde150ff265f5ba2068274863c5329de8
R-Owino <sayeakinyi@gmail.com>
2025-01-15 23:38:58 +0300

    refactor: file download route: - remove API gateway download process logic - instead, generate a pre-signed URL to handle direct file download from S3

--- app/FlaskApp/routes/download.py
+++ app/FlaskApp/routes/download.py
@@ -1,40 +1,84 @@
-from flask import Blueprint, request, jsonify, session, redirect
-import requests
+import os
+import boto3
+import logging
 from config import Config
+from urllib.parse import unquote_plus
+from botocore.exceptions import ClientError
+from flask import Blueprint, jsonify, request, session
+
+logger = logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+s3 = boto3.client("s3")
+BUCKET_NAME = Config.S3_BUCKET_NAME
 
 download_bp = Blueprint("download", __name__)
-
-AWS_API_GATEWAY_DOWNLOAD_URL = Config.AWS_API_GATEWAY_DOWNLOAD_URL
 
 @download_bp.route("/download", methods=["GET"])
 def download_file():
     """
-    Requests a pre-signed URL for downloading a file from S3
-    Passes thru file_key directly to the lambda function
+    Generates a pre-signed URL for downloading a file from S3
     """
+    logger.info("Download route accessed")
+
     if "username" not in session:
         return jsonify({"error": "Unauthorized"}), 401
     
     try:
-        # prepare headers with auth
-        headers = {"Authorization": f"Bearer {session.get.access_token}"}
+        file_key = unquote_plus(request.args.get("file_key", ""))
 
-        # forward the query string to the download lambda function
-        response = requests.get(
-            AWS_API_GATEWAY_DOWNLOAD_URL,
-            headers=headers,
-            params=request.args
-        )
-
-        if response.status_code == 200:
-            presigned_url = response.json().get("presigned_url")
-            if presigned_url:
-                return redirect(presigned_url)
-            else:
-                return jsonify({"error": "No pre-signed URL generated"}), 500
-        else:
+        if not file_key:
+            logger.info("Missing file key")
             return jsonify({
-                "error": f"Failed to generate download URL: {response.text}"
-            }), response.status_code
-    except requests.RequestException as e:
-        return jsonify({"error": f"Network error: {str(e)}"}), 500
+                "error": "Missing required parameter: file_key"
+            }), 400
+
+        # exract the original file name
+        base_name = os.path.basename(file_key)
+        file_name = '-'.join(base_name.split('-')[5:])
+        
+        # check if the file exists in the bucket
+        try:
+            s3.head_object(
+                Bucket=BUCKET_NAME,
+                Key=file_key
+            )
+        except ClientError as e:
+            logger.error(f"S3 error: {str(e)}")
+            if e.response["Error"]["Code"] == "404":
+                return jsonify({
+                    "error": "File not found"
+                }), 404
+            logger.error(f"S3 error: {str(e)}")
+            return jsonify({
+                "error": "Error accessing file"
+            }), 500
+        
+        # generate a pre-signed url with 1 hour expiration time
+        try:
+            presigned_url = s3.generate_presigned_url(
+                "get_object",
+                Params={
+                    "Bucket": BUCKET_NAME,
+                    "Key": file_key,
+                    "ResponseContentDisposition": \
+                        f'attachment; filename="{file_name}"'
+                },
+                ExpiresIn=3600
+            )
+            logger.info(f"Generated pre-signed URL: {presigned_url}")
+
+            return jsonify({
+                "presigned_url": presigned_url
+            }), 200
+
+        except ClientError as e:
+            logger.error(f"Error generating pre-signed URL: {str(e)}")
+            return jsonify({
+                "error": "Failed to generate download URL"
+            }), 500
+    except Exception as e:
+        logger.error(f"Unexpected error: {str(e)}")
+        return jsonify({
+            "error": "An unexpected error occured"
+        }), 500
