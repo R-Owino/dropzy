97e6558b43c516ad08a6adbe2a46f7291b966441
R-Owino <sayeakinyi@gmail.com>
2025-01-15 23:50:35 +0300

    refactor: add upload lamda function name and ARN

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -7,3 +7,13 @@ variable "environment" {
   description = "Environment (dev/prod)"
   type        = string
 }
+
+variable "upload_lambda_arn" {
+  description = "Invoke ARN of the upload lambda function"
+  type        = string
+}
+
+variable "upload_lambda_function_name" {
+  description = "Name of the upload lambda function"
+  type        = string
+}
