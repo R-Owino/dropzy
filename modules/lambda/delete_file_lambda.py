b5565c9f190b8e5abf1988f501f47e204630625f
R-Owino <sayeakinyi@gmail.com>
2025-01-16 23:49:50 +0300

    refactor: specify the DocumentId as the key for deleting in DynamoDB

--- modules/lambda/delete_file_lambda.py
+++ modules/lambda/delete_file_lambda.py
@@ -6,23 +6,27 @@ import os
 import logging
 from botocore.exceptions import ClientError
 
+s3 = boto3.client("s3")
+dynamodb = boto3.resource("dynamodb")
+
+BUCKET_NAME = os.environ["S3_BUCKET_NAME"]
+TABLE_NAME = os.environ["DYNAMODB_TABLE_NAME"]
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
     try:
-        s3 = boto3.client("s3")
-        dynamodb = boto3.resource("dynamodb")
-
-        BUCKET_NAME = os.environ['BUCKET_NAME']
-        TABLE_NAME = os.environ['TABLE_NAME']
-
+        # get the file key from sent event
         file_key = event.get('queryStringParameters', {}).get('file_key')
+        logger.info(f"Received file_key: {file_key}")
 
         if not file_key:
             return {
                 'statusCode': 400,
-                'body': json.dumps({'error': 'Missing file_key'})
+                'body': json.dumps({
+                    'error': 'Missing file_key'
+                })
             }
         
         # delete form s3
@@ -32,7 +36,9 @@ def lambda_handler(event, context):
             logger.error(f"S3 deleteion error: {e}")
             return {
                 'statusCode': 500,
-                'body': json.dumps({'error': 'Failed to delete from S3'})
+                'body': json.dumps({
+                    'error': 'Failed to delete from S3'
+                })
             }
         
         # delete metadata from DynamoDB
@@ -46,17 +52,21 @@ def lambda_handler(event, context):
 
             items = response.get('Items', [])
             if items:
-                table.delete_item(Key={'file_id': items[0]['file_id']})
+                table.delete_item(Key={'DocumentId': items[0]['DocumentId']})
         except ClientError as e:
             logger.error(f"DynamoDB deleteion error: {e}")
             return {
                 'statusCode': 500,
-                'body': json.dumps({'error': 'Failed to delete from DynamoDB'})
+                'body': json.dumps({
+                    'error': 'Failed to delete from DynamoDB'
+                })
             }
         
         return {
             'statusCode': 200,
-            'body': json.dumps({'message': 'File deleted successfully'})
+            'body': json.dumps({
+                'message': 'File deleted successfully'
+            })
         }
     except Exception as e:
         logger.error(f"Unexpected error: {e}")
