0677c9bb6ca6a068521c6485c007e77edecb794e
R-Owino <sayeakinyi@gmail.com>
2025-02-02 21:23:11 +0300

    update: output lambda functions invoke ARNs to be used in API gateway

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -1,4 +1,4 @@
-/* lambda functions outputs */
+/* lambda module outputs */
 
 output "userdata_function_arn" {
   description = "ARN of the userdata lambda function"
@@ -25,9 +25,14 @@ output "file_metadata_function_name" {
   value       = aws_lambda_function.fetch_file_metadata.function_name
 }
 
-output "file_metadata_arn" {
+output "file_metadata_function_arn" {
   description = "ARN of the file metadata lambda function"
   value       = aws_lambda_function.fetch_file_metadata.arn
+}
+
+output "file_metadata_invoke_arn" {
+  description = "Invoke ARN of the file metadata lambda function"
+  value       = aws_lambda_function.fetch_file_metadata.invoke_arn
 }
 
 output "delete_lambda_function_name" {
@@ -38,6 +43,11 @@ output "delete_lambda_function_name" {
 output "delete_function_arn" {
   description = "ARN of the delete file lambda function"
   value       = aws_lambda_function.delete_file.arn
+}
+
+output "delete_lambda_invoke_arn" {
+  description = "Invoke ARN of the delete lambda function"
+  value       = aws_lambda_function.delete_file.invoke_arn
 }
 
 output "lambda_execution_role_arn" {
