868c0e72ea59a2bcee8f445bab66b47c73e8028d
R-Owino <sayeakinyi@gmail.com>
2025-02-03 22:49:48 +0300

    chore: remove message flashing

--- app/FlaskApp/routes/main.py
+++ app/FlaskApp/routes/main.py
@@ -3,7 +3,6 @@ from flask import (Blueprint,
                    render_template,
                    redirect,
                    url_for,
-                   flash,
                    session)
 
 logging.basicConfig(level=logging.DEBUG)
@@ -18,14 +17,11 @@ def main():
 
     logger.debug("Session logged_in: %s", session.get("logged_in"))
     logger.debug("Session username: %s", session.get("username"))
-    logger.debug("Session email: %s", session.get("email"))
-    logger.debug("Session access_token: %s", session.get("access_token"))
+    logger.debug("id_token: %s", session.get("id_token"))
 
     if not session.get("logged_in"):
         logger.warning("Unauthorized access attempt to main page")
-        flash("Please log in first.", "warning")
         return redirect(url_for("login.login"))
     
     return render_template("main.html",
-                           username=session["username"],
-                           email=session["email"])
+                           username=session["username"])
