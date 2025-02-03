ea08c5a11297682c26869a4d6f579d79ba4e7569
R-Owino <sayeakinyi@gmail.com>
2025-02-03 23:57:04 +0300

    update: add parameter that fetches the 15 most recent files

--- app/FlaskApp/routes/file_metadata.py
+++ app/FlaskApp/routes/file_metadata.py
@@ -1,8 +1,7 @@
 import requests
+from . import file_metadata_bp
 from config import Config
-from flask import Blueprint, jsonify, session
-
-file_metadata_bp = Blueprint("file_metadata", __name__)
+from flask import jsonify, session
 
 AWS_API_GATEWAY_FETCH_METADATA_URL = Config.AWS_API_GATEWAY_FETCH_METADATA_URL
 
@@ -10,16 +9,18 @@ AWS_API_GATEWAY_FETCH_METADATA_URL = Config.AWS_API_GATEWAY_FETCH_METADATA_URL
 def file_metadata():
     """
     Fetch recent file metadata from documents DynamoDB table
-    via API gateway
+    via API gateway with optional limit
     """
     if "username" not in session:
         return jsonify({"error": "Unauthorized"}), 401
 
     try:
         headers = {"Authorization": f"Bearer {session.get('id_token')}"}
+        params = {"limit": 15}
         response = requests.get(
             AWS_API_GATEWAY_FETCH_METADATA_URL,
-            headers=headers
+            headers=headers,
+            params=params
         )
 
         if response.status_code == 200:
