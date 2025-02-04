ded928339349676a5ff02c11129fcac3985c1d87
R-Owino <sayeakinyi@gmail.com>
2025-02-05 01:24:27 +0300

    update: conform to PEP8 linting

---
+++ api/v1/config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    """app configurations"""
+    SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "xyz-xyz-xyz")
+    AWS_REGION = os.getenv("AWS_REGION", "us-west-2")
+    AWS_COGNITO_USER_POOL_ID = os.getenv("AWS_COGNITO_USER_POOL_ID")
+    AWS_COGNITO_CLIENT_ID = os.getenv("AWS_COGNITO_CLIENT_ID")
+    API_GATEWAY_INVOKE_URL = os.getenv("AWS_API_GATEWAY_INVOKE_URL")
+    AWS_API_GATEWAY_FETCH_METADATA_URL = os.getenv("AWS_API_GATEWAY_FETCH_METADATA_URL")  # noqa: F401
+    AWS_API_GATEWAY_DELETE_URL = os.getenv("AWS_API_GATEWAY_DELETE_URL")
+    S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+    DYNAMODB_TABLE_NAME = os.getenv("DYNAMODB_TABLE_NAME")
