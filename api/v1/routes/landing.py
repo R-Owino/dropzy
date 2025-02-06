ac7f45effc30bd24611633021081a67ffab8df87
R-Owino <sayeakinyi@gmail.com>
2025-02-06 23:43:59 +0300

    docs: add docstring for landing page rendering logic

--- api/v1/routes/landing.py
+++ api/v1/routes/landing.py
@@ -1,9 +1,19 @@
 from flask import Blueprint, render_template
 
+# Define Blueprint for the landing page route
 landing_bp = Blueprint("landing", __name__)
 
 
 @landing_bp.route("/")
 def landing():
-    """defines the landing page route"""
+    """
+    Handle requests to the landing page
+
+    GET:
+        - Render index.html template as the landing page
+
+    Returns:
+        JSON response:
+            - 200 OK: landing page content
+    """
     return render_template("index.html")
