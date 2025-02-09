48747473340b966d798a5868dbb5fbb514e21f0f
R-Owino <sayeakinyi@gmail.com>
2025-02-09 22:48:09 +0300

    refactor: aggregate AWS services under one directory

---
+++ infra/providers.tf
@@ -0,0 +1,14 @@
+# configure AWS provider
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.aws_region
+  profile = "default"
+}
