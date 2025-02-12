f60b96714b25a5ab11f57ae37328724b2abbe4d1
R-Owino <sayeakinyi@gmail.com>
2025-02-12 23:51:59 +0300

    refactor: change the output names of the upload file metadata function

--- infra/modules/lambda/outputs.tf
+++ infra/modules/lambda/outputs.tf
@@ -10,14 +10,14 @@ output "userdata_function_name" {
   value       = aws_lambda_function.userdata.function_name
 }
 
-output "upload_function_name" {
-  description = "Name of the upload Lambda function"
-  value       = aws_lambda_function.upload.function_name
+output "upload_metadata_function_name" {
+  description = "Name of the file metadata uploading lambda function"
+  value       = aws_lambda_function.upload_file_metadata.function_name
 }
 
-output "upload_function_arn" {
-  description = "ARN of the upload lambda function"
-  value       = aws_lambda_function.upload.arn
+output "upload_metadata_function_arn" {
+  description = "ARN of the file metadata uploading lambda function"
+  value       = aws_lambda_function.upload_file_metadata.arn
 }
 
 output "file_metadata_function_name" {
