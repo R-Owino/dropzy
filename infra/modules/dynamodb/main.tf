4f77b92c1b479a44ab31996d222000bdb7dfa326
R-Owino <sayeakinyi@gmail.com>
2025-02-20 23:15:24 +0300

    update: remove terraform_lock table, placed in boostrap config

--- infra/modules/dynamodb/main.tf
+++ infra/modules/dynamodb/main.tf
@@ -41,14 +41,3 @@ resource "aws_dynamodb_table" "documents_metadata" {
 
 }
 
-# for state locking
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "terraform_lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
