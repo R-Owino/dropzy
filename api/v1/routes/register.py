c6d1fdfcc3f6dc38a86c5a0ab50e070b76743044
R-Owino <sayeakinyi@gmail.com>
2025-02-21 19:45:22 +0300

    update: add a method email_exists that checks if the email address being registered already exists in the userpool

--- api/v1/routes/register.py
+++ api/v1/routes/register.py
@@ -3,9 +3,14 @@ from flask import (Blueprint,
                    request,
                    redirect,
                    url_for,
-                   session
+                   session,
+                   jsonify
                    )
-from v1.cognito import register_user
+from v1.cognito import register_user, email_exists
+import logging
+
+logging.basicConfig(level=logging.ERROR)
+logger = logging.getLogger(__name__)
 
 # Define Blueprint for register user route
 register_bp = Blueprint("register", __name__)
@@ -29,18 +34,29 @@ def register():
         JSON response:
             - 200 OK: render the registration page
             - 302 Redirect: confirmation page upon successful registration
+            - 400 Bad Request: registration failed due to invalid inputs
             - 409 Conflict: email entered is already registered
+            - 500 Internal Server Error: server error during registration
     """
     if request.method == "POST":
         email = request.form.get("email")
         username = request.form.get("username")
         password = request.form.get("password")
 
-        result = register_user(email, username, password)
-        if result["Success"]:
-            session["verification_email"] = email
-            return redirect(url_for("api.confirm.confirm"))
-        elif "UsernameExistsException" in result.get("message", ""):
-            return render_template("register.html"), 409
+        try:
+            if email_exists(email):
+                return jsonify({"message": "User with email already exists."}), 409
+
+            result = register_user(email, username, password)
+            if result["Success"]:
+                session["verification_email"] = email
+                return redirect(url_for("api.confirm.confirm"))
+            else:
+                return jsonify({
+                    "message": "Registration failed. Please check your inputs."
+                }), 400
+        except Exception as e:
+            logger.error(f"Registration error: {e}")
+            return jsonify({"message": "An error occured. Please try again."}), 500
 
     return render_template("register.html")
